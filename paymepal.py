eb189187df1f8bd0556b4343fbba6633681b09c8
Pepe García <pepe@pepegar.com>
2024-03-05 22:25:44 +0100

    prepare code for class

--- paymepal.py
+++ paymepal.py
@@ -20,46 +20,12 @@ def index():
 
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.form["user"]
-    password = request.form["password"]
-
-    query = f"""
-        SELECT *
-        FROM users
-        WHERE username='{username}' AND password='{password}';
-        """
-
-    with engine.connect() as connection:
-        row = connection.execute(query).fetchone()
-
-        if row:
-            session["username"] = username
-            session["user_id"] = row[0]
-
-            return redirect("/admin")
-        else:
-            return redirect("/unauthorized")
+    pass
 
 
 @app.route("/admin")
 def admin():
-    query = f"""
-        SELECT *
-        FROM transactions t
-        INNER JOIN shops s
-        ON t.shop_id = s.id
-        WHERE user_id='{session["user_id"]}'
-        """
-
-    with engine.connect() as connection:
-        results = connection.execute(query).fetchall()
-
-        return render_template("private.html", transactions=results)
-
-
-@app.route("/unauthorized")
-def unauthorized():
-    return render_template("unauthorized.html")
+    pass
 
 
 app.run(debug=True, port=8080)
