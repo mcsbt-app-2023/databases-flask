734af66d75dd528031d7e83392306f4406e2f696
Pepe García <pepe@pepegar.com>
2024-03-07 19:16:52 +0100

    add code from class

--- paymepal.py
+++ paymepal.py
@@ -1,6 +1,6 @@
 import hashlib
 from flask import Flask, render_template, request, session, redirect
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "very secret stuff"
@@ -20,7 +20,28 @@ def index():
 
 @app.route("/login", methods=["POST"])
 def login():
-    pass
+    user = request.form["user"]
+    password = hash_value(request.form["password"])
+
+    query = text(
+        f"""
+        SELECT username, password
+        FROM users
+        WHERE username=:username AND password=:password
+        """
+    )
+    user_found = False
+
+    with engine.connect() as connection:
+        results = connection.execute(query, username=user, password=password)
+
+        for user in results:
+            user_found = True
+
+    if user_found:
+        return "OK"
+    else:
+        return "user not found", 403
 
 
 @app.route("/admin")
